6fde2ae880bb9ba0ff308c6063c3f497a45a4129
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-10-07 02:46:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Product CRUD App
+
+* simple project to add & read update delete by react
+
+## libarys
+
+* libary used in this project: react react-dom reac-Hooks bootstrap uuid 
---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  return (
+    <>
+      <section className="navbar">
+      
+      </section>
+      <section className="main-pagr">
+      
+      </section>
+      <section className="footer">
+
+      </section>
+
+    </>
+  );
+}
+
+export default App;
