20c24a37bd3c8ba533b05ae6c88074708fc7b74c
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-10-09 04:24:23 +0300

    Tooltip massege

--- src/component/Products.js
+++ src/component/Products.js
@@ -1,7 +1,7 @@
 import { useContext, useState, useEffect } from "react"
 import { ProductContext } from "../contexts/ProductContext"
 import EditForm from "./EditForm"
-import { Modal, Button } from "react-bootstrap"
+import { Modal, Button, OverlayTrigger, Tooltip } from "react-bootstrap"
 
 const Product = (props) => {
     const {product} = props
@@ -22,14 +22,22 @@ const Product = (props) => {
             <td>{ product.desc }</td>
             <td>{ product.price } <span className="SYP">SYP</span></td>
             <td>
-
-                <button onClick={handleShow}  className="btn text-warning btn-act" data-toggle="modal">
-                    <i className="material-icons">&#xE254;</i>
-                </button>
-
-                <button onClick={() => deleteProduct(product.id)}  className="btn text-danger btn-act" data-toggle="modal">
-                    <i className="material-icons">&#xE872;</i>
-                </button>
+            <OverlayTrigger
+                    overlay={
+                        <Tooltip id={`tooltip-top`}>
+                            Edit
+                        </Tooltip>
+                    }>
+                    <button onClick={handleShow}  className="btn text-warning btn-act" data-toggle="modal"><i className="material-icons">&#xE254;</i></button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                    overlay={
+                        <Tooltip id={`tooltip-top`}>
+                            Delete
+                        </Tooltip>
+                    }>
+                    <button onClick={() => deleteProduct(product.id)}  className="btn text-danger btn-act" data-toggle="modal"><i className="material-icons">&#xE872;</i></button>
+                </OverlayTrigger>
             </td>
 
             <Modal show={show} onHide={handleClose}>
